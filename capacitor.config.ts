6c89ab2bdbf6d6ac58560cf610c19cd87acc6808
imagenhlayiso <imagenhlayiso@gmail.com>
2022-06-06 14:11:35 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'maliwa-hhs',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
